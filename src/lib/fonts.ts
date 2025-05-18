dfbf6dba4d76f7909226eecf244c0ac22c288fbb
Jimmy Chu <25955797+jimmyclchu@users.noreply.github.com>
2025-05-18 11:48:46 -0400

    fix: font path

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,10 +1,10 @@
 import localFont from "next/font/local";
 
 export const gtAmericaMono = localFont({
-  src: "/GT-America-Mono-Regular.woff2",
-  variable: "--font-gt-america-mono"
+  src: "../../public/GT-America-Mono-Regular.woff2",
+  variable: "--font-gt-america-mono",
 });
 export const gtAmerica = localFont({
-  src: "/GT-America-Standard-Regular.woff2",
+  src: "../../public/GT-America-Standard-Regular.woff2",
   variable: "--font-gt-america",
 });
