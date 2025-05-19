a48f1e8c5c4d7a28d94d7ea476ba9ecc7e7364a9
Jimmy Chu <25955797+jimmyclchu@users.noreply.github.com>
2025-05-19 11:42:24 -0400

    fix: apple music developer token path

--- src/lib/apple-music/get-now-playing.ts
+++ src/lib/apple-music/get-now-playing.ts
@@ -3,7 +3,7 @@ export async function getNowPlaying(userToken: string) {
 
   const response = await fetch(`${endpoint}`, {
     headers: {
-      "Authorization": `Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6IjQ4SkRNV0IyTFgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJDNlpTOU5ONTVTIiwiZXhwIjoxNzQ3NjU4NDg1LCJpYXQiOjE3NDc2MTUyODV9.IXzCnVvKihqJABXFnnZU4ktrKwooMurvDqRoW2TrTBnVd4YEm79jq-5eXk7zGh0WKF4lFRexG0IhUKFxUnHHuA`,
+      "Authorization": `Bearer ${process.env.NEXT_PUBLIC_APPLE_MUSIC_DEVELOPER_TOKEN}`,
       "Music-User-Token": userToken,
     },
   });
