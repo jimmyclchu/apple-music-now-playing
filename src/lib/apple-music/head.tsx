fec2f5575dd80d27ce1c112ad0f0a92f1ee1165e
Jimmy Chu <25955797+jimmyclchu@users.noreply.github.com>
2025-05-19 13:50:11 -0400

    fix: musickit script defer

--- src/lib/apple-music/head.tsx
+++ src/lib/apple-music/head.tsx
@@ -1,7 +1,7 @@
 export function AppleMusicHead() {
   return (
     <>
-      <script src="https://js-cdn.music.apple.com/musickit/v3/musickit.js" async></script>
+      <script src="https://js-cdn.music.apple.com/musickit/v3/musickit.js" async defer></script>
       <meta name="apple-music-developer-token" content={process.env.NEXT_PUBLIC_APPLE_MUSIC_DEVELOPER_TOKEN} />
       <meta name="apple-music-app-name" content={process.env.NEXT_PUBLIC_APPLE_MUSIC_APP_NAME} />
       <meta name="apple-music-app-build" content="1.0.0" />
