eaeaca9299e5b464194cebaf176f9762377c0be8
Jimmy Chu <25955797+jimmyclchu@users.noreply.github.com>
2025-05-19 00:20:38 -0400

    feat: url change animation

--- src/components/ui/AppURL.tsx
+++ src/components/ui/AppURL.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles/AppURL.module.css";
+import { useEffect, useState } from "react";
 
 export default function AppURL({
   url,
@@ -7,12 +8,27 @@ export default function AppURL({
   url: string,
   onCopy: () => void,
 }) {
+  const [characters, setCharacters] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Split by empty string to preserve all characters including spaces
+    setCharacters([...url]);
+  }, [url]);
+
   return <div
     className={styles.url}
     onClick={onCopy}
   >
     <div className={styles.urlText}>
-      {url}
+      {characters.map((char, index) => (
+        <span 
+          key={`${char}-${index}`} 
+          className={styles.character}
+          style={{ animationDelay: `${index * 0.005}s` }}
+        >
+          {char}
+        </span>
+      ))}
     </div>
     <div className={styles.tail}></div>
   </div>;
